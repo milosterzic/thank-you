0e2391827d5b7705734c4d8260a750dd54a87ae6
Milos Terzic <terzic.milos.tm@gmail.com>
2019-12-30 21:29:05 +0100

    adding donations table

---
+++ database/migrations/2019_12_30_201556_create_donations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies');
+
+            $table->unsignedBigInteger('philanthropist_id');
+            $table->foreign('philanthropist_id')->references('id')->on('philanthropists');
+
+            $table->timestamp('thanked_at', 0)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
