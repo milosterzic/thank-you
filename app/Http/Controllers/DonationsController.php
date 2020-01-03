ca7dd3ff75506ae4ae91655651c47cc1c953f4bd
Milos Terzic <terzic.milos.tm@gmail.com>
2020-01-03 14:51:03 +0100

    sending notification

--- app/Http/Controllers/DonationsController.php
+++ app/Http/Controllers/DonationsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Philanthropist;
+use App\Notifications\PhilanthropistHelped;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Request;
 
 class DonationsController extends Controller
 {
@@ -23,6 +26,10 @@ class DonationsController extends Controller
      */
     public function notify() : JsonResponse
     {
+        $philanthropist = Philanthropist::find(Request::get('philanthropist_id'));
+
+        $philanthropist->notify(new PhilanthropistHelped);
+
         // TODO Write down notification logic and call it here.
 
         return response()->json([
